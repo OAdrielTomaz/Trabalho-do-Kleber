da45043e7ff0dadba44aaf1078f744907ec5825d
Adriel Tomaz <adriel.tomaz@escola.pr.gov.br>
2025-09-01 11:08:21 -0300

    Update quantidadeUsuario.js

--- grafico/quantidadeUsuario.js
+++ grafico/quantidadeUsuario.js
@@ -1,1 +1,26 @@
+async function quantidadeUsuario() {
+    const url = "https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json"
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDosPostos = Object.keys(dados)
+const quantidadeTrabalhadores = Object.values(dados)
+
+
+const data = [
+    {
+        x: nomeDosPostos,
+        y:quantidadeTrabalhadores,
+        type: 'bar'
+    }
+]
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(graficos)
+Plotly.newPlot(graficos, data)
+
+}
+
+quantidadeUsuario()
+
 
